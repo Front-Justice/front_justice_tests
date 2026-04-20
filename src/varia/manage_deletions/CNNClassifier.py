6b7a9e048db9db8921e7f9d7cc7fa09ce8ba2218
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2026-04-20 10:03:14 -0400

    Update resize

--- src/varia/manage_deletions/CNNClassifier.py
+++ src/varia/manage_deletions/CNNClassifier.py
@@ -205,13 +205,13 @@ DATA_DIR = arguments.input_dir
 # --- 1. Définir les paramètres ---
 # DATA_DIR = "/media/mgl/stock/Front_Justice/data/HTR_data/data/main_text/extracted/data/lines_splits/"
 IMAGE_SIZE = (65, 1500)  # Taille cible (après padding/redimensionnement)
-# IMAGE_SIZE = (65, 65)  # Taille cible (après padding/redimensionnement)
+IMAGE_SIZE = (28, 135)  # Taille cible (après padding/redimensionnement)
 NUM_CLASSES = 2  # À adapter selon votre nombre de classes
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 transform = transforms.Compose([
-    #transforms.Resize(IMAGE_SIZE),
+    transforms.Resize(IMAGE_SIZE),
     transforms.CenterCrop(IMAGE_SIZE),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.5], std=[0.5]),  # Normaliser à [-1, 1]
