1b08b6c20b9cf28558ceedbc8ace95a31ce4c879
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2026-05-07 12:54:15 +0200

    Remove print statemeent

--- src/acquire.py
+++ src/acquire.py
@@ -847,8 +847,6 @@ def main(images_dir:str,
 	if start_after:
 		images = [item for item in images  if int(item.split("/")[-1].replace(".jpg", "")) > start_after]
 		images.sort(key=lambda x: int(x.split("/")[-1].split(".jpg")[0]))
-		print(images)
-		exit(0)
 	else:
 		target = None
 	try:
