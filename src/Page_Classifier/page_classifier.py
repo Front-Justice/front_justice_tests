3a1ca1adaa9a6438704959ddfa02a05688a6776d
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2026-05-08 21:35:46 +0200

    Reduce vertical crop factor to add more information

--- src/Page_Classifier/page_classifier.py
+++ src/Page_Classifier/page_classifier.py
@@ -104,7 +104,7 @@ class PageClassifier():
 		image = self.load_to_greyscale(image_path)
 		if show_image:
 			Image.Image.show(image)
-		cropped = self.crop_and_resize(image, vertical_crop_factor=4)
+		cropped = self.crop_and_resize(image, vertical_crop_factor=2)
 		features = self.extract_features(cropped)
 		if produce_labels:
 			return features, self.reverse_vocab.get(image_path.split("/")[-2])
