f2d19897b435398cb9fde6d481e9b19c8cf7a785
Matthias Gille Levenson <matthias.gille-levenson@ens-lyon.fr>
2026-05-11 15:58:38 +0200

    Update reconciliation

--- src/Information_Extractor/reconciliation.py
+++ src/Information_Extractor/reconciliation.py
@@ -65,16 +65,12 @@ class Reconciliator:
 		print("---")
 
 	def _check_minute_consistency(self):
-		classes = [item["classe"].split("_")[-1] for item in self.minute_list if item["classe"] != "page_autre"]
-		print(classes)
-		try:
-			if range(int(classes[0]), int(classes[-1]) + 1) == range(1, 5):
-				print("Minute correctement ordonnée")
-				return True
-			else:
-				print("Quelque chose ne va pas avec la minute")
-				return False
-		except IndexError:
+		classes = [int(item["classe"].split("_")[-1]) for item in self.minute_list if item["classe"] != "page_autre"]
+		if classes == [1, 2, 3, 4]:
+			print("Minute correctement ordonnée")
+			return True
+		else:
+			print("Quelque chose ne va pas avec la minute")
 			return False
 
 	def _reconciliate_questions(self):
